0a0abd42dedb4e44c94926fce00ceaddf2e7974a
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2024-11-03 14:46:03 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,3 +1,27 @@
+from pyrogram import filters
+from devgagan import app
+from devgagan.core import script
+from devgagan.core.func import subscribe
+from config import OWNER_ID
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+
+# ------------------- Start-Buttons ------------------- #
+
+buttons = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton("Join Channel", url="https://t.me/TARGETALLCOURSE")],
+        [InlineKeyboardButton("Buy Premium", url="https://t.me/targetallcourse")]
+    ]
+)
+
+@app.on_message(filters.command("start"))
+async def start(_, message):
+    join = await subscribe(_, message)
+    if join == 1:
+        return
+    await message.reply_photo(photo="https://iili.io/2qVlgTu.md.jpg",
+                              caption=script.START_TXT.format(message.from_user.mention), 
+                              reply_markup=buttons)
 import os
 from config import Config
 
