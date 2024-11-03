f085bc9fa9756cbee32f6bfe051e63bc2449ac07
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2024-11-03 14:23:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,9 @@ class Config:
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
+    CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002182291545"))
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '902551614').split()]
-
+    
 class temp(object): 
     lock = {}
     CANCEL = {}
